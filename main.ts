51b2fcacf65b083f51cb7b31c60a161c9bc6ab94
chranychaul <130452965+chranychaul@users.noreply.github.com>
2023-05-22 09:24:08 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
 input.onButtonPressed(Button.A, function () {
+    basic.showNumber(aceptar)
     aceptar = 0
 })
 input.onGesture(Gesture.Shake, function () {
     aceptar += 1
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    basic.showNumber(77 * aceptar)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    aceptar = 0
+})
 let aceptar = 0
 aceptar = 0
-basic.forever(function () {
-    basic.showNumber(aceptar)
-})
